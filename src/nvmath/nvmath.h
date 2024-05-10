e0669867ac28c0aca9c24dd3e51cb9b645cc8275
acidicMercury8 <acidicmercury8@outlook.com>
2024-05-10 13:04:39 +0300

    Delete conflicted functions for MSVC
    Co-authored-by: ForserX <sv3nk@yandex.ru>

--- src/nvmath/nvmath.h
+++ src/nvmath/nvmath.h
@@ -117,18 +117,6 @@ inline float asinf_assert(const float f)
 #define asinf asinf_assert
 #endif
 
-#if NV_CC_MSVC
-NV_FORCEINLINE float log2f(float x)
-{
-    nvCheck(x >= 0);
-    return logf(x) / logf(2.0f);
-}
-NV_FORCEINLINE float exp2f(float x)
-{
-    return powf(2.0f, x);
-}
-#endif
-
 namespace nv
 {
     inline float toRadian(float degree) { return degree * (PI / 180.0f); }
